098c1925432c1ff69c974c6fac6a6819c0526371
hearo_nakata <nakahii@msn.com>
2019-10-27 21:21:55 +0900

    test

---
+++ src/main/java/jp/org/web/controller/UpdateController.java
@@ -0,0 +1,134 @@
+package jp.org.web.controller;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jp.org.web.form.LanguageForm;
+import jp.org.web.form.LessonlistForm;
+import jp.org.web.repository.LanguageRepository;
+import jp.org.web.repository.LessonListRepository;
+
+
+
+/**
+ * Handles requests for the application home page.
+ */
+
+//コントローラであることを記載するアノテーション
+@Controller
+public class UpdateController {
+
+	private static final Logger logger = LoggerFactory.getLogger(UpdateController.class);
+
+	@Autowired
+	private LessonListRepository lessonlistrepository;
+
+	@Autowired
+	private LanguageRepository languagerepository;
+
+	@ModelAttribute
+	public LessonlistForm setlessonlistForm() {
+		LessonlistForm lessonlistForm = new LessonlistForm();
+		return lessonlistForm;
+	}
+
+	@ModelAttribute
+	public LanguageForm setlanguageForm() {
+		LanguageForm languageForm = new LanguageForm();
+		return languageForm;
+	}
+
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/02_update/update/{id}", method = RequestMethod.GET)
+	public String home(Locale locale, Model model, @PathVariable String id) {
+		logger.info("Update screen display");
+		logger.info("userId -> " + id);
+
+		List<LanguageForm> languageForm = languagerepository.getlanguagelist();
+		model.addAttribute("language", languageForm);
+
+		logger.info("language -> " + languageForm.get(0).getLanguage());
+
+		LessonlistForm lessonDataForm = lessonlistrepository.getLessonData(id);
+		model.addAttribute("LessonListForm", lessonDataForm);
+
+		return "/02_update/update";
+	}
+
+	//新規ボタン押下で画面遷移してきた時のメソッド
+	@RequestMapping(value = "/02_update/update/addRow", method = RequestMethod.GET)
+	public String initNewRow(Locale locale, Model model) {
+		logger.info("Update screen display new row");
+
+		String newUserId = String.format("%03d", this.getAddRowNo());
+		LessonlistForm lessonDataForm = new LessonlistForm();
+		lessonDataForm.setUserId(newUserId);
+		lessonDataForm.setInsertFlg(true);
+		model.addAttribute("LessonListForm", lessonDataForm);
+
+		List<LanguageForm> languageForm = languagerepository.getlanguagelist();
+		model.addAttribute("language", languageForm);
+
+		return "/02_update/update";
+	}
+
+
+
+	@RequestMapping(value = "/02_update/update/{path}", method = RequestMethod.POST)
+	public String updateData(@PathVariable String path, Model model, LessonlistForm lessonlistForm, RedirectAttributes attr) {
+		logger.info("update data");
+
+		//初期値としてupdateを持たせる
+		String funcType = "update";
+
+		//削除の場合
+		if(lessonlistForm.isDeleteFlg()) {
+			lessonlistrepository.deleteLessonData(lessonlistForm.getUserId());
+		//deleteを代入
+		funcType = "delete";
+		//新規作成の場合
+		} else if(lessonlistForm.isInsertFlg()) {
+			lessonlistrepository.insert(lessonlistForm.getUserId(), lessonlistForm.getUserFirstName(), lessonlistForm.getUserLastName(), lessonlistForm.getLesson1st(), lessonlistForm.getLesson2nd());
+			//insertを代入
+			funcType = "insert";
+		}else {
+			lessonlistrepository.update(lessonlistForm.getUserId(), lessonlistForm.getUserFirstName(), lessonlistForm.getUserLastName(), lessonlistForm.getLesson1st(), lessonlistForm.getLesson2nd());
+		}
+
+		//URLのパラメータで更新か削除か新規かをセットする
+		attr.addAttribute("funcType", funcType);
+
+		return "redirect:/01_list/list";
+
+	}
+
+	//空いてるuserId探すメソッド
+	private int getAddRowNo() {
+		List<LessonlistForm> list = lessonlistrepository.getLessonListMap();
+		int newRowId = 1;
+		for(LessonlistForm lessonForm: list) {
+			int userIdInt = Integer.parseInt(lessonForm.getUserId());
+			if(userIdInt != newRowId) {
+				return newRowId;
+			}
+			newRowId++;
+		}
+
+		return newRowId;
+	}
+
+}
---
+++ src/main/java/jp/org/web/repository/LessonListRepository.java
@@ -0,0 +1,43 @@
+package jp.org.web.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Component;
+
+import jp.org.web.form.LessonlistForm;
+
+@Component
+public interface LessonListRepository {
+    @Select("select * from samurai_lessonlist")
+    List<LessonlistForm> getLessonListMap();
+
+	@Select("select * from samurai_lessonlist where userId = #{userId}")
+	LessonlistForm getLessonData(@Param("userId") String userId);
+
+	@Insert("insert into samurai_lessonlist values(#{userId}, #{userFirstName}, #{userLastName}, #{lesson1st}, #{lesson2nd})")
+	void insert(@Param("userId") String userId
+			,@Param("userFirstName") String userFirstName
+			,@Param("userLastName") String userLastName
+			,@Param("lesson1st") String lesson1st
+			,@Param("lesson2nd") String lesson2nd);
+
+    @Update("update samurai_lessonlist set userFirstName = #{userFirstName}"
+            + ", userLastName = #{userLastName}"
+            + ", lesson1st = #{lesson1st}"
+            + ", lesson2nd = #{lesson2nd}"
+            + " where userId = #{userId}")
+			void update(@Param("userId") String userId
+							,@Param("userFirstName") String userFirstName
+							,@Param("userLastName") String userLastName
+							,@Param("lesson1st") String lesson1st
+							,@Param("lesson2nd") String lesson2nd);
+
+	@Delete("delete from samurai_lessonlist where userId = #{userId}")
+	void deleteLessonData(@Param("userId") String userId);
+
+}
